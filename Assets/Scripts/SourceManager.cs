5f87a558618cc5e7c9c9de699f0ffe606e79a5e6
Cyross Makoto <takuan45189@gmail.com>
2020-02-04 08:04:30 +0900

    first commit

---
+++ Assets/Scripts/SourceManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SourceManager : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject sourcePrefab;
+    public int numsOfObject;
+    public float radius;
+    public float playInterval;
+
+    private GameObject[] objects;
+    private int obj_index;
+    private float countInterval;
+
+    private void Awake()
+    {
+        Vector3 basePosition = mainCamera.transform.position;
+        objects = new GameObject[numsOfObject];
+        for (int i=0; i<numsOfObject; i++)
+        {
+            GameObject gobj = Instantiate(sourcePrefab);
+            float radian = 2.0f * Mathf.PI / (float)numsOfObject * (float)i;
+            gobj.transform.position = new Vector3(basePosition.x + radius * Mathf.Cos(radian),  basePosition.y, basePosition.z + radius * Mathf.Sin(radian));
+            objects[i] = gobj;
+        }
+        obj_index = 0;
+        countInterval = 0.0f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlaySound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        countInterval += Time.deltaTime;
+        if(countInterval >= playInterval)
+        {
+            PlaySound();
+            countInterval = 0.0f;
+        }
+    }
+
+    void PlaySound()
+    {
+        objects[obj_index].GetComponent<TestSoundPlayer>().PlaySound();
+        obj_index = (obj_index + 1) % numsOfObject;
+    }
+}
---
+++ Assets/Scripts/TestSoundPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSoundPlayer : MonoBehaviour
+{
+    private AudioSource source;
+    private AudioClip clip;
+    private bool is_start;
+
+    public GameObject shpere;
+    public Material[] materials;
+
+    public  int  playing_material_index;
+
+    private void Awake()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        clip = source.clip;
+        is_start = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        if (is_start)
+        {
+            if (!source.isPlaying)
+            {
+                FinalSound();
+            }
+        }
+    }
+
+    void FinalSound()
+    {
+        shpere.GetComponent<Renderer>().material = materials[0];
+        is_start = false;
+    }
+
+    public void PlaySound()
+    {
+        shpere.GetComponent<Renderer>().material = materials[playing_material_index];
+        source.Play();
+        is_start = true;
+    }
+}
